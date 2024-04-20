b0dd77f273e48ae5eac317aade04a23616c7e100
Darlisson <darlisson.dev@gmail.com>
2024-04-20 17:39:14 -0300

    Refactor BearerAuthenticationProviderTest

--- notes-app-server/src/test/java/com/br/notesapp/notesappserver/config/BearerAuthenticationProviderTest.java
+++ notes-app-server/src/test/java/com/br/notesapp/notesappserver/config/BearerAuthenticationProviderTest.java
@@ -1,43 +1,40 @@
 package com.br.notesapp.notesappserver.config;
 
+import com.br.notesapp.notesappserver.MongoTestContainer;
 import com.br.notesapp.notesappserver.dto.auth.TokenPayload;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 
 @SpringBootTest
-class BearerAuthenticationProviderTest {
+class BearerAuthenticationProviderTest extends MongoTestContainer {
 
-
+    @MockBean
     AccessTokenProvider tokenProvider;
 
-    UserDetails user;
-
-    @BeforeEach
-    void setUp() {
-        user = User.builder().username("john").password("123").authorities("EMPLOYEE").build();
-        tokenProvider = Mockito.mock(AccessTokenProvider.class);
-    }
+    @Autowired
+    BearerAuthenticationProvider bearerAuthenticationProvider;
 
     @Test
     void shouldAuthenticateUserWithSuccess() {
         // given
+        var user = User.builder().username("john").password("123").authorities("EMPLOYEE").build();
         var request = new MockHttpServletRequest();
         Mockito.when(tokenProvider.resolve(request)).thenReturn("access_token");
         Mockito.when(tokenProvider.verify("access_token")).thenReturn(new TokenPayload("john", List.of("EMPLOYEE")));
-        var authenticationProvider = new BearerAuthenticationProvider(tokenProvider);
 
         // when
-        var authentication = authenticationProvider.authenticateUser(request);
+        var authentication = bearerAuthenticationProvider.authenticateUser(request);
 
+        // then
         Assertions.assertEquals("john", authentication.getName());
         Assertions.assertEquals(List.of(new SimpleGrantedAuthority("EMPLOYEE")), authentication.getAuthorities());
 
