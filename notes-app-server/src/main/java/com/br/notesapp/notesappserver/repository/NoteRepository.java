c4a3c794d7c71a6cda4595d74ec076062a916ec7
Darlisson <darlisson.dev@gmail.com>
2024-04-21 16:13:47 -0300

    Fix Bug

--- notes-app-server/src/main/java/com/br/notesapp/notesappserver/repository/NoteRepository.java
+++ notes-app-server/src/main/java/com/br/notesapp/notesappserver/repository/NoteRepository.java
@@ -2,7 +2,6 @@ package com.br.notesapp.notesappserver.repository;
 
 import com.br.notesapp.notesappserver.model.Note;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +9,6 @@ import java.util.List;
 @Repository
 public interface NoteRepository extends MongoRepository<Note, String> {
 
-    @Query("{'user': ?0}")
-    List<Note> findAllByUserId(String userId);
+    List<Note> findByUserId(String userId);
 
 }
