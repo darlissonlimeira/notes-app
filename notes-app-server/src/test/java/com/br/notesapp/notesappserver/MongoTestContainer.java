3ec63d3b7d962c4814d7fe5d219685a8570cc0e7
Darlisson <darlisson.dev@gmail.com>
2024-04-21 16:13:14 -0300

    Fix Tests

--- notes-app-server/src/test/java/com/br/notesapp/notesappserver/MongoTestContainer.java
+++ notes-app-server/src/test/java/com/br/notesapp/notesappserver/MongoTestContainer.java
@@ -1,20 +1,36 @@
 package com.br.notesapp.notesappserver;
 
+import com.br.notesapp.notesappserver.model.Note;
+import com.br.notesapp.notesappserver.model.UserModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+@SpringBootTest
 @Testcontainers
 public class MongoTestContainer {
 
     @Container
     static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0").withExposedPorts(27017);
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     @DynamicPropertySource
     static void setMongoDBContainer(DynamicPropertyRegistry registry) {
         mongoDBContainer.start();
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @BeforeEach
+    void setUp() {
+        mongoTemplate.dropCollection(UserModel.class);
+        mongoTemplate.dropCollection(Note.class);
     }
 }
