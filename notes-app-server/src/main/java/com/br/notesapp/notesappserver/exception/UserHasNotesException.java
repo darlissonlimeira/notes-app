092c8f44bd35b29b4dafaeb15b53f717e0883ab6
Darlisson <darlisson.dev@gmail.com>
2024-04-21 16:14:11 -0300

    General Refactor

--- notes-app-server/src/main/java/com/br/notesapp/notesappserver/exception/UserHasNotesException.java
+++
@@ -1,7 +0,0 @@
-package com.br.notesapp.notesappserver.exception;
-
-public class UserHasNotesException extends RuntimeException {
-    public UserHasNotesException(String message) {
-        super(message);
-    }
-}
